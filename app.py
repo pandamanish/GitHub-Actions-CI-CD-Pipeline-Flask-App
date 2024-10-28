cac533890d90b4747bc09d91e48c3efc71500903
pandamanish <57584893+pandamanish@users.noreply.github.com>
2024-10-29 00:35:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+@app.route('/add/<int:a>/<int:b>')
+def add(a, b):
+    return str(a + b)
+
+if __name__ == '__main__':
+    app.run()
